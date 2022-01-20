5d40c43dd284e9839b948bf1545fbc915bd578db
kentownley <96872554+kentownley@users.noreply.github.com>
2022-01-20 17:48:26 +0800

    git commit

--- loginAPI.js
+++ loginAPI.js
@@ -1,43 +1,14 @@
-  // Fetch User
-  const fetchUser = async ({username, password}) => {
-    const res = await fetch(`http://localhost:5000/login/`, {body:JSON.stringify(data)}), 
-    const data = await res.json()
+function handleClick({username, password})
 
-    return data
-  }
+document.write(document.getElementById('username').value);
+document.write(document.getElementById('password').value);
 
-
-
- 
-const element = document.querySelector('#post-request .article-id');
 const requestOptions = {
 method: 'POST',
 headers: { 'Content-Type': 'application/json' },
-body: JSON.stringify({username:'username', password:'password'})
-};
+body: JSON.stringify({username:document.getElementById('username').value, password:document.getElementById('password').value
+})};
+
 fetch('http://localhost:5000/login/', requestOptions)
 .then(response => response.json())
 .then(data => element.innerHTML = data.id );
-
-
-
-
-
-
-  body: JSON.stringify(`http://localhost:5000/login/${username}/${password}`)
-
-    // Add User
-    const addUser = async (user) => {
-      const res = await fetch('http://localhost:5000/login', {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      })
-  
-      const data = await res.json()
-  
-      setUser([...user, data])
-
-    }

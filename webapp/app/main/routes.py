d551b9f2bb14cff3fc97cc58ed3010cef660e9ff
93acy <acy_071093@hotmail.com>
2022-01-20 16:16:41 +0800

    add login endpoint and secure get_post endpoint

--- webapp/app/main/routes.py
+++ webapp/app/main/routes.py
@@ -1,10 +1,11 @@
-from typing import Dict
+from flask import request
 from flask_restx import Resource
+from werkzeug.exceptions import Unauthorized
 
 from app.main import api
-from app.auth import generate_token
-from app.models import Post, User
-import json
+from app.auth import login, generate_token, login_required
+from app.models import Post
+
 
 @api.route('/health')
 class HealthCheck(Resource):
@@ -13,26 +14,32 @@ class HealthCheck(Resource):
         token = generate_token('asdasd')
         return {'status': 'service is up and running', 'token': token}, 200
 
-@api.route('/test')
-class Test(Resource):
+@api.route('/login')
+class Login(Resource):
     @api.doc(responses={200: 'OK', 400: 'Errors'})
-    def get(self):
-        a = User.query.filter_by(Name='Brose McCreery').first()
-        print(a)
-        print(type(a))
-        print(type(a.to_dict))
-        print(type(a.to_dict()))
-        print(a.to_dict())
-        return a.to_dict(), 200
+    def post(self):
+        data = request.json
+        if not data:
+            raise Unauthorized('username or password not provided')
+        if 'username' in data and 'password' in data:
+            valid, user_id = login(data['username'], data['password'])
+            if valid:
+                token = generate_token(data['username'], user_id)
+                return {"token": token}, 200
+            else:
+                raise Unauthorized('invalid username or password')
+        else:
+            raise Unauthorized('username or password not provided')
 
 @api.route('/get_post')
-class getAllPosts(Resource):
+class GetAllPosts(Resource):
     @api.doc(responses={200: 'OK', 400: 'Errors'})
-    def get(self):
+    @login_required
+    def get(self, payload):
         a = Post.query.all()
         list =[]
         for x in a:
             list.append(x.to_dict())
-            # print(x.to_dict())
        
         return list, 200
+        

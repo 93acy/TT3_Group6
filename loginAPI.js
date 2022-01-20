aca2fd2bc9de948c8ca217f3205fe7ec291bdf92
kentownley <96872554+kentownley@users.noreply.github.com>
2022-01-20 16:38:22 +0800

    git commit

--- loginAPI.js
+++ loginAPI.js
@@ -1,10 +1,30 @@
   // Fetch User
   const fetchUser = async ({username, password}) => {
-    const res = await fetch(`http://localhost:5000/login/${username}${password}`)
+    const res = await fetch(`http://localhost:5000/login/`, {body:JSON.stringify(data)}), 
     const data = await res.json()
 
     return data
   }
+
+
+
+ 
+const element = document.querySelector('#post-request .article-id');
+const requestOptions = {
+method: 'POST',
+headers: { 'Content-Type': 'application/json' },
+body: JSON.stringify({username:'username', password:'password'})
+};
+fetch('http://localhost:5000/login/', requestOptions)
+.then(response => response.json())
+.then(data => element.innerHTML = data.id );
+
+
+
+
+
+
+  body: JSON.stringify(`http://localhost:5000/login/${username}/${password}`)
 
     // Add User
     const addUser = async (user) => {

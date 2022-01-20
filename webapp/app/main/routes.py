530bd1529db6205354f82ddd06bc75676d0b7110
93acy <acy_071093@hotmail.com>
2022-01-20 17:07:24 +0800

    add endpoint to create post

--- webapp/app/main/routes.py
+++ webapp/app/main/routes.py
@@ -55,3 +55,25 @@ class GetUserPost(Resource):
             p = Post.query.filter_by(Post_ID=likedpost.Post_ID).first()
             list.append(p.to_dict())
         return list, 200
+
+@api.route('/create-post')
+class CreatePost(Resource):
+    @api.doc(responses={200: 'OK', 400: 'Errors'})
+    @login_required
+    def post(payload, self):
+        current_user_id = payload['User_ID']
+        data = request.json
+        
+        if 'Post_Title' not in data or 'Post_Description' not in data or 'Post_image' not in data:
+            return {"message": "Post_Title, Post_Descriptionor Post_image not provided"}, 400
+
+        p = Post(Post_Title=data['Post_Title'], Post_Description=data['Post_Description'], Post_image=data['Post_image'])
+        session = db.session()
+        session.add(p)
+        session.commit()
+
+        lp = LikedPost(User_ID=current_user_id, Post_ID=p.Post_ID)
+        session = db.session()
+        session.add(lp)
+        session.commit()
+        return {"message": "post created", "post_id": p.Post_ID}, 201
